b8317fb6afe6991fcf2ed5609c77ecd36399751a
Kaan Gokcek <48089870+gokcekaan97@users.noreply.github.com>
2023-01-09 16:47:05 +0300

    new project

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var MyLabel: UILabel!
+ 
+    @IBOutlet weak var CharImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func ChangeClicked(_ sender: Any) {
+        CharImage.image = UIImage(named: "char2")
+    }
+    
 
 }
 
